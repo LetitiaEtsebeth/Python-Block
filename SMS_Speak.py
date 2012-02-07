052268dec47fa0eaa3758c30dbfd4e570c9cda0d
Letitia Etsebeth <15597601@sun.ac.za>
2012-02-08 00:17:34 +0200

    Added timing

--- SMS_Speak.py
+++ SMS_Speak.py
@@ -1,16 +1,20 @@
 __author__ = '15597601'
-import re                  
-vow_punc = re.compile(r'(\B[aeuoi]\B)|\W')          # Matches non-alphanumeric character and all vowels not on the word boundaries           
-doubles = re.compile(r'([a-z])\1')                  # Matches the same alphabetic chars next to each other
+import re, time
 
 text = raw_input('Enter some text: ')
+start_time = time.time()                            # Set a variable with the starting time
+
+vow_punc = re.compile(r'(\B[aeuoi]\B)|\W')          # Matches non-alphanumeric character and all vowels not on the word boundaries           
+doubles = re.compile(r'([a-z])\1')                  # Matches the same alphabetic chars next to each other
 
 text = text.lower()                                 # Changes the text into all lowercase
 words = text.split()                                # Creates a list
 stripwords = []
 for word in words:                                  # For every word
-    strip = vow_punc.sub('',word)                   # Remove punctuation and the vowels
-    strip = doubles.sub(r'\1',strip)     # If duplicates found, replace it with one of the same character
+    strip = vow_punc.sub('',word)                   # Replace punctuation and the vowels with an empty string
+    strip = doubles.sub(r'\1',strip)                # Replace duplicates with one of the same character
     stripwords.append(strip)
 
 print ' '.join(stripwords)                          # Display the new list with spaces in between.
+
+print time.time() - start_time, "seconds"           # Minus the current time from the start time
