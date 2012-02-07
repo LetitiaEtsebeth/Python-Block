bcf660c39ee703fb607459ea435238806b865894
Letitia Etsebeth <15597601@sun.ac.za>
2012-02-07 23:59:20 +0200

    Single RE for both Vowels and Punctuation

--- SMS_Speak.py
+++ SMS_Speak.py
@@ -1,24 +1,21 @@
 __author__ = '15597601'
-import re
-spaces = re.compile(r'\s')                          # Matches whitespace chars
-punctuation = re.compile(r'\W')                     # Matches non-alphanumeric character
-vowels = re.compile(r'\B[aeuoi]\B')                 # Matches all vowels not on the word boundaries
+import re                  
+vow_punc = re.compile(r'(\B[aeuoi]\B)|\W')          # Matches non-alphanumeric character and all vowels not on the word boundaries           
 doubles = re.compile(r'([a-z])\1')                  # Matches the same alphabetic chars next to each other
 
 text = raw_input('Enter some text: ')
 text = text.lower()                                 # Changes the text into all lowercase
 
-words = spaces.split(text)                          # Creates a list, separating words based on whitespace
+words = text.split()                                # Creates a list
 stripwords = []
 for word in words:                                  # For every word
-    nopunct = punctuation.sub('',word)              # Remove punctuation
-    novowels = vowels.sub('',nopunct)               # Remove the vowels
-    double = doubles.search(novowels)               # Search for duplicate chars
+    strip = vow_punc.sub('',word)                   # Remove punctuation and the vowels
+    double = doubles.search(strip)               # Search for duplicate chars
     if double:
-        nodoubles = doubles.sub(r'\1',novowels)     # If duplicates found, replace it with one of the same character
+        nodoubles = doubles.sub(r'\1',strip)     # If duplicates found, replace it with one of the same character
         stripwords.append(nodoubles)                # Add the modified word to a new list
     else:
-        stripwords.append(novowels)
+        stripwords.append(strip)
 
 
 print ' '.join(stripwords)                          # Display the new list with spaces in between.
