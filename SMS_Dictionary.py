dae6308f6a55b013f1d8ef878ca1dc1039c33a1c
Letitia Etsebeth <15597601@sun.ac.za>
2012-02-08 22:13:51 +0200

    Decoding

--- SMS_Dictionary.py
+++ SMS_Dictionary.py
@@ -1,6 +1,6 @@
 __author__ = '15597601'
 import cPickle as pickle                                    # Import the module to steralize & desteralize objects
-import re, time
+import re
 vow_punc = re.compile(r'(\B[aeuoi]\B)|[^a-zA-Z0-9\s]')      # Matches non-alphanumeric and spaces chars or vowels not on the word boundaries           
 doubles = re.compile(r'([a-z])\1')                          # Matches the same alphabetic chars next to each other
 
@@ -20,6 +20,5 @@ for line in inputfile:
         sms_dict.update({sms(word):word})                   # Translates the word to sms and then adds it to the dictionary if the key can not be found
 
 pickle.dump(sms_dict, open( "Dictionary.p", "wb" ) )        # 'Writes' the dictionary object to an external file
-print len(sms_dict)
 
 inputfile.close()
