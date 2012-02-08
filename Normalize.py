ef008248e20ee94da918d0b179ca59d69be1f439
Letitia Etsebeth <15597601@sun.ac.za>
2012-02-08 19:30:09 +0200

    With newline chars

--- Normalize.py
+++ Normalize.py
@@ -2,22 +2,16 @@ name = raw_input('Enter the file name: ')
 newname = raw_input('Enter the name to create the file: ')
 
 file = open(name)
-newfile = open(newname,'w')                     # Creates a newfile in which to store output
+newfile = open(newname,'w')                                 # Creates a newfile in which to store output
 
 for line in file:
-    line = line.lower()                         # Only adds the words to a list
+    line = line.lower()                                     # Only adds the words to a list
     linelist = line.split()
 
-    if linelist:                                # If the list is not empty it writes it to the new file with a single space in between
-        newfile.write(' '.join(linelist)+' ')
+    if linelist:                                            # If the list is not empty it writes it to the new file with a single space in between
+        newfile.write(' '.join(linelist)+'\n')               # Writes to the new file
 
 file.close()
 newfile.close()
-
-read = open(newname)
-for line in read:
-    print line
-
-read.close()
 
 
