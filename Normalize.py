2179ab301ed9cf5b5e96f5c60866e226458ffacc
Letitia Etsebeth <15597601@sun.ac.za>
2012-02-08 16:24:17 +0200

    Normalizing spaces

---
+++ Normalize.py
@@ -0,0 +1,11 @@
+name = raw_input('Enter the file name: ')
+newname = raw_input('Enter the name to create the file: ')
+
+file = open(name)
+newfile = open(newname,'w')                     # Creates a newfile in which to store output
+
+for line in file:
+    linelist = line.split()                     # Only adds the words to a list             
+    if linelist:                                # If the list is not empty it writes it to the new file with a single space in between
+        newfile.write(' '.join(linelist)+' ')
+
