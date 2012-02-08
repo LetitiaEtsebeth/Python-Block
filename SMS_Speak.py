e10dde0960fac3825ed6ca6b34ee0500aed4b8a4
Letitia Etsebeth <15597601@sun.ac.za>
2012-02-08 14:13:31 +0200

    Removed "word"-for and changed puncuatuation pattern

--- SMS_Speak.py
+++ SMS_Speak.py
@@ -1,20 +1,18 @@
 __author__ = '15597601'
-import re, time
+import re, time, string
 
-text = raw_input('Enter some text: ')
-start_time = time.time()                            # Set a variable with the starting time
-
-vow_punc = re.compile(r'(\B[aeuoi]\B)|\W')          # Matches non-alphanumeric character and all vowels not on the word boundaries           
+vow_punc = re.compile(r'(\B[aeuoi]\B)|[^a-zA-Z0-9\s]')          # Matches non-alphanumeric character and all vowels not on the word boundaries           
 doubles = re.compile(r'([a-z])\1')                  # Matches the same alphabetic chars next to each other
 
-text = text.lower()                                 # Changes the text into all lowercase
-words = text.split()                                # Creates a list
+book = open('pg76.txt')
+start_time = time.clock()                            # Set a variable with the starting time
 stripwords = []
-for word in words:                                  # For every word
-    strip = vow_punc.sub('',word)                   # Replace punctuation and the vowels with an empty string
+for text in book:
+    text = text.lower().strip()                             # Changes the text into all lowercase
+    strip = vow_punc.sub('',text)                   # Replace punctuation and the vowels with an empty string
     strip = doubles.sub(r'\1',strip)                # Replace duplicates with one of the same character
-    stripwords.append(strip)
+    print strip
 
-print ' '.join(stripwords)                          # Display the new list with spaces in between.
+book.close()
 
-print time.time() - start_time, "seconds"           # Minus the current time from the start time
+print time.clock() - start_time, "seconds"           # Minus the current time from the start time'''
